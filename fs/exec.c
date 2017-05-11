6b3a729d307b39d2fc9025f9ba3d41a377df63bd
Daniel Micay <danielmicay@gmail.com>
2017-05-11 16:02:49 -0400

    randomize lower bits of the argument block
    This was based on the PaX RANDUSTACK feature in grsecurity, where all of
    the lower bits are randomized. PaX keeps 16-byte alignment.
    
    maade: as part of porting to 6.1, get_random_int() was replaced by get_random_u32()
    
    Signed-off-by: Daniel Micay <danielmicay@gmail.com>
    [levente@leventepolyak.net: do not randomize with ADDR_NO_RANDOMIZE personality]
    Signed-off-by: Levente Polyak <levente@leventepolyak.net>
    Signed-off-by: anupritaisno1 <www.anuprita804@gmail.com>
    Signed-off-by: randomhydrosol <randomhydrosol@glassrom.org>

--- fs/exec.c
+++ fs/exec.c
@@ -70,6 +70,7 @@
 #include <linux/rseq.h>
 #include <linux/ksm.h>
 #include <linux/dma-buf.h>
+#include <linux/random.h>
 
 #include <linux/uaccess.h>
 #include <asm/mmu_context.h>
@@ -296,6 +297,8 @@ static int __bprm_mm_init(struct linux_binprm *bprm)
 	mm->stack_vm = mm->total_vm = 1;
 	mmap_write_unlock(mm);
 	bprm->p = vma->vm_end - sizeof(void *);
+	if (!(current->personality & ADDR_NO_RANDOMIZE) && randomize_va_space)
+		bprm->p ^= get_random_u32() & ~PAGE_MASK;
 	return 0;
 err:
 	ksm_exit(mm);
